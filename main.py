efe93be45328997505460c5e9f093bb64e16b203
root <root@beaglebone.localdomain>
2018-10-28 16:43:29 +0000

    upgrade to python 3

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from __future__ import print_function
 import Adafruit_BBIO.UART as UART
 import serial
-
+import time
+import sys
 #UART.setup("UART1");
 
-ser = serial.Serial(port = "/dev/ttyO1", baudrate=9600)
+ser = serial.Serial(port = "/dev/ttyO1", baudrate=9600, timeout=1.0)
 ser.close()
+
 ser.open()
 ch = ""
 dist = 0;
 while ser.isOpen():
-	c = ser.read()
-	if c == '\r':
-		dist = float(ch)
-		ch = ""
-		print("Distance is %.1f cm" % dist)
-		continue
-	elif c in ['0','1','2','3','4','5','6','7','8','9']:
-		ch += c
-
-	print("%s" % c, end="")
-
+	try:
+		ch = ser.read_until(b'\r')
+#		print(ch)
+		ch = ch.decode('utf-8', 'ignore')
+		dist = float("".join(x for x in ch if x in ".1234567890"))
+		print("Dist = %.1f cm" % dist)
+		sys.stdout.flush()
+	except ValueError:
+		print("err: %s" % ch)
 UART.cleanup();
